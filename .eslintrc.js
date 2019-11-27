989e4e55740c9b728aa013976ff4195b5da6260a
Bryan Chen <xlchen1291@gmail.com>
2019-11-28 11:55:15 +1300

    basic project boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+const base = require('@polkadot/dev/config/eslint');
+
+module.exports = {
+  ...base,
+  parserOptions: {
+    ...base.parserOptions,
+    project: [
+      './tsconfig.json'
+    ]
+  },
+  rules: {
+    ...base.rules
+  }
+};
