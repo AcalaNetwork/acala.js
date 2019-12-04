0e0d4e913ce323d32d6833b36996321bd725fab7
Bryan Chen <xlchen1291@gmail.com>
2019-12-05 11:29:06 +1300

    add generate script

---
+++ packages/types/scripts/generate.ts
@@ -0,0 +1,22 @@
+import { generateInterfaceRegistry } from '@polkadot/types/scripts/generateTypes/interfaceRegistry';
+import { generateTsDef } from '@polkadot/types/scripts/generateTypes/tsDef';
+import * as defaultDefinations from '@polkadot/types/interfaces/definitions';
+
+import * as ormlDefinations from '@orml/types/interfaces/definitions';
+
+import * as acalaDefinations from '../src/interfaces/definitions';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { runtime, deprecated, ...substrateDefinations } = defaultDefinations;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { runtime: _runtime, ...ormlModulesDefinations } = ormlDefinations;
+
+const definations = {
+  '@polkadot/types/interfaces': substrateDefinations,
+  '@orml/types/interfaces': ormlModulesDefinations,
+  '@acala-network/types/interfaces': acalaDefinations
+};
+
+generateTsDef(definations, 'packages/types/src/interfaces', '@acala-network/types/interfaces');
+generateInterfaceRegistry(definations, 'packages/types/src/interfaceRegistry.ts');
---
+++ packages/types/src/index.ts
@@ -0,0 +1,5 @@
+import { assertSingletonPackage } from '@polkadot/util';
+
+export { InterfaceRegistry } from './interfaceRegistry';
+
+assertSingletonPackage('@orml/types');
