8ba58d87a976a7903e8753f7e63c021114daf399
qiuhao <qiuhao951@gmail.com>
2020-03-25 01:01:30 +0800

    update loan&dex help methods

--- packages/utils/src/dex.ts
+++ packages/utils/src/dex.ts
@@ -1,30 +1,112 @@
 import { FixedU128 } from './fixed-u128';
 
-export function calcCanGenerater (
-  collateralAmount: FixedU128,
-  debitAmount: FixedU128,
-  requiredCollateralRatio: FixedU128,
-  stableCoinPrice: FixedU128
-): FixedU128 {
-  if (requiredCollateralRatio.isZero() || stableCoinPrice.isZero()) {
-    return FixedU128.ZERO;
-  }
-  // sub 0.00001 to ensure generate success
-  const result = collateralAmount
-    .div(requiredCollateralRatio)
-    .sub(debitAmount)
-    .div(stableCoinPrice)
-    .sub(FixedU128.fromNatural(0.00001));
-  return result.isNaN() ? FixedU128.ZERO : result;
+interface Pool {
+  other: FixedU128;
+  base: FixedU128;
 }
 
-export function calcLiquidationPrice (
-  collateral: FixedU128,
-  debitAmount: FixedU128,
-  liquidationRatio: FixedU128
+/**
+ * @name calcReceive
+ * @description calculate how many target token can be received when use the supply token.
+ * Equation: received = (1 - slippage) * (1 - fee) * (targetPool - (supplyPool * targetPool) / (supplyPool + supply))
+ * @param {FixedU128} supply - supply token quantity
+ * @param {FixedU128} supplyPool - supply token quantity in the pool
+ * @param {FixedU128} targetPool - target token quantity in the pool
+ * @param {FixedU128} fee - swap fee
+ * @param {FixedU128} [slippage=0] - swap slippage
+ * @returns {FixedU128} return target token quantity which can be received
+ */
+export function calcReceive (
+  supply: FixedU128,
+  supplyPool: FixedU128,
+  targetPool: FixedU128,
+  fee: FixedU128,
+  slippage = FixedU128.ZERO
 ): FixedU128 {
-  if (debitAmount.isZero()) {
-    return FixedU128.ZERO;
-  }
-  return debitAmount.mul(liquidationRatio).div(collateral);
+  const newTargetPool = supplyPool.mul(targetPool).div(supplyPool.add(supply));
+  const receivedQuantity = targetPool.sub(newTargetPool);
+  const receivedRatio = FixedU128.fromNatural(1).sub(slippage).mul(FixedU128.fromNatural(1).sub(fee));
+  return receivedQuantity.mul(receivedRatio);
+}
+
+/**
+ * @name calcSupply
+ * @description calculate how many supply token required for the target token.
+ * Equation: supply = supplyPool - (supplyPool * targetPool) / (targetPool - (target / (1 - fee) / (1 - slippage)))
+ * @param {FixedU128} target - target token quantity
+ * @param {FixedU128} supplyPool - supply token quantity in the pool
+ * @param {FixedU128} targetPool - target token quantity in the pool
+ * @param {FixedU128} fee - swap fee
+ * @param {FixedU128} [slippage=0] - swap slippage
+ * @returns {FixedU128} target token quantity can be received
+ */
+export function calcSupply (
+  target: FixedU128,
+  supplyPool: FixedU128,
+  targetPool: FixedU128,
+  fee: FixedU128,
+  slippage = FixedU128.ZERO
+): FixedU128 {
+  const receivedRatio = FixedU128.fromNatural(1).sub(slippage).mul(FixedU128.fromNatural(1).sub(fee));
+  const realyTargetQuantity = target.div(receivedRatio);
+  const newSupplyPool = supplyPool.mul(targetPool).div(targetPool.sub(realyTargetQuantity));
+  return newSupplyPool.sub(supplyPool);
+}
+
+/**
+ * @name calcReceivedOtherToOther
+ * @description calculate how many target token can be received by another pool.other token.
+ * Equation: target = (1 - slippage) * calcReceivedOtherByBase(calcReceivedBaseByOther(target, targetPool, fee), receivePool, fee)
+ * Note: This method will calculate fee twice
+ * @param {FixedU128} target  - target token quantity
+ * @param {FixedU128} targetPool.target - target token quantity in target pool
+ * @param {FixedU128} targetPool.base - base token quantity in target pool
+ * @param {FixedU128} receivePool.target - target token quantity in receive pool
+ * @param {FixedU128} receivePool.base - base token quantity in receive pool
+ * @param {FixedU128} fee - swap fee
+ * @param {FixedU128} [slippage=0] - swap slippage
+ * @returns {FixedU128} target token quantity which can be received in the receive pool
+ */
+export function calcReceivedOtherToOther (
+  target: FixedU128,
+  targetPool: Pool,
+  receivePool: Pool,
+  fee: FixedU128,
+  slippage = FixedU128.ZERO
+): FixedU128 {
+  // don't calculate slippage in calcOtherToBase swap
+  const receivedBaseQuantityInOtherPool = calcReceive(target, targetPool.other, targetPool.base, fee);
+  // don't calculate slippage in calcBaseToOther swap
+  const receivedOtherQuantityInReceivePool = calcReceive(receivedBaseQuantityInOtherPool, receivePool.base, receivePool.other, fee);
+  // calculate slippage
+  return receivedOtherQuantityInReceivePool.mul(FixedU128.fromNatural(1).sub(slippage));
+}
+
+/**
+ * @name calcRequiredOtherToOther
+ * @description calculate how many other pool.other token required for target token.
+ * Equation: target = (1 - slippage) * calcRequiredOtherForBase(calcRequreidBaseForOther(target, targetPool, fee), receivePool, fee)
+ * Note: This method will calculate fee twice
+ * @param {FixedU128} target  - target token quantity
+ * @param {FixedU128} targetPool.target - target token quantity in target pool
+ * @param {FixedU128} targetPool.base - base token quantity in target pool
+ * @param {FixedU128} receivePool.target - target token quantity in receive pool
+ * @param {FixedU128} receivePool.base - base token quantity in receive pool
+ * @param {FixedU128} fee - swap fee
+ * @param {FixedU128} [slippage=0] - swap slippage
+ * @returns {FixedU128} target token quantity which can be received in the receive pool
+ */
+export function calcRequiredOtherToOther (
+  target: FixedU128,
+  targetPool: Pool,
+  receivePool: Pool,
+  fee: FixedU128,
+  slippage = FixedU128.ZERO
+): FixedU128 {
+  // don't calculate slippage in calcOtherToBase swap
+  const requiredBaseQuantityInPool = calcSupply(target, targetPool.other, targetPool.base, fee);
+  // don't calculate slippage in calcBaseToOther swap
+  const requiredOtherQuantityInReceivePool = calcSupply(requiredBaseQuantityInPool, receivePool.base, receivePool.other, fee);
+  // calculate slippage
+  return requiredOtherQuantityInReceivePool.mul(FixedU128.fromNatural(1).sub(slippage));
 }
--- packages/utils/src/loan.ts
+++ packages/utils/src/loan.ts
@@ -1,75 +1,232 @@
 import { FixedU128 } from './fixed-u128';
 
-const ZERO = FixedU128.ZERO;
-
-export function debitToUSD (debit: FixedU128, debitExchangeRate: FixedU128, stableCoinPrice: FixedU128): FixedU128 {
-  return debit.mul(debitExchangeRate).mul(stableCoinPrice);
+/**
+ * @name debitToStableCoin
+ * @description transform debit to stable coin
+ * @param {FixedU128} quantity - user loan debit quantity
+ * @param {FixedU128} debitExchangeRate - loan debit exchange rate
+ * @returns {FixedU128} return the stable coin quantity
+ */
+export function debitToStableCoin (quantity: FixedU128, debitExchangeRate: FixedU128): FixedU128 {
+  return quantity.mul(debitExchangeRate);
 }
 
-export function USDToDebit (
-  stableValue: FixedU128,
-  debitExchangeRate: FixedU128,
-  stableCoinPrice: FixedU128
-): FixedU128 {
-  if (stableCoinPrice.isZero() || debitExchangeRate.isZero()) {
-    return ZERO;
-  }
-  return stableValue.div(stableCoinPrice).div(debitExchangeRate);
-}
-
-export function debitToStableCoin (debit: FixedU128, debitExchangeRate: FixedU128): FixedU128 {
-  return debit.mul(debitExchangeRate);
-}
-
+/**
+ * @name stableCoinToDebit
+ * @description transform stable coin to debit
+ * @param {FixedU128} amount - stable coin amount
+ * @param {FixedU128} debitExchangeRate - loan debit exchange rate
+ * @returns {FixedU128} return the debit quantity
+ */
 export function stableCoinToDebit (amount: FixedU128, debitExchangeRate: FixedU128): FixedU128 {
-  return amount.div(debitExchangeRate);
+  return debitExchangeRate.isZero ? FixedU128.ZERO : amount.div(debitExchangeRate);
 }
 
-export function collateralToUSD (collateral: FixedU128, collateralPrice: FixedU128): FixedU128 {
-  return collateral.mul(collateralPrice);
+/**
+ * @name debitToUSD
+ * @description transform debit to USD
+ * @param {FixedU128} quantity - user loan debit quantity
+ * @param {FixedU128} debitExchangeRate - loan debit exchange rate
+ * @param {FixedU128} stableCoinPrice - stable coin price
+ * @returns {FixedU128} return the debit amount denominated in USD
+ */
+export function debitToUSD (quantity: FixedU128, debitExchangeRate: FixedU128, stableCoinPrice: FixedU128): FixedU128 {
+  return debitToStableCoin(quantity, debitExchangeRate).mul(stableCoinPrice);
 }
 
+/**
+ * @name USDToDebit
+ * @description transform USD to debit
+ * @param {FixedU128} amount - USD amount
+ * @param {FixedU128} debitExchangeRate - loan debit exchange rate
+ * @param {FixedU128} stableCoinPrice - stable coin price
+ * @returns {FixedU128} return the debit quantity
+ */
+export function USDToDebit (amount: FixedU128, debitExchangeRate: FixedU128, stableCoinPrice: FixedU128): FixedU128 {
+  return stableCoinPrice.isZero() ? FixedU128.ZERO : stableCoinToDebit(amount.div(stableCoinPrice), debitExchangeRate);
+}
+
+/**
+ * @name collateralToUSD
+ * @description transform collateral to USD
+ * @param {FixedU128} quantity - collateral quantity
+ * @param {FixedU128} collateralPrice - collateral price
+ * @returns {FixedU128} return the collateral amount denominated in USD
+ */
+export function collateralToUSD (quantity: FixedU128, collateralPrice: FixedU128): FixedU128 {
+  return quantity.mul(collateralPrice);
+}
+
+/**
+ * @name USDToCollateral
+ * @description transform USD to collateral
+ * @param {FixedU128} amount - USD amount
+ * @param {FixedU128} collateralPrice - collateral price
+ * @returns {FixedU128} return the collateral quantity
+ */
+export function USDToCollateral (amount: FixedU128, collateralPrice: FixedU128): FixedU128 {
+  return amount.div(collateralPrice);
+}
+
+/**
+ * @name calcCollateralRatio
+ * @description calculate collateral ratio. Equation: collateralRatio = collateralAmount : debitAmount
+ * @param {FixedU128} collateralAmount - collateral amount (USD). Equation: collateralAmount = collateral * collateralPrice
+ * @param {FixedU128} debitAmount - debit amount (USD). Equation: debitAmount = debit * debitExchangeRate * stableCoinPrice
+ * @returns {FixedU128} return collateral ratio
+ */
 export function calcCollateralRatio (collateralAmount: FixedU128, debitAmount: FixedU128): FixedU128 {
-  if (debitAmount.isZero()) {
-    return ZERO;
-  }
-  return collateralAmount.div(debitAmount);
+  return debitAmount.isZero() ? FixedU128.ZERO : collateralAmount.div(debitAmount);
 }
 
-const YEAR = 364 * 24 * 60 * 60; // second of one yera
-export function calcStableFee (stableFee: FixedU128, blockTime: number): FixedU128 {
+const YEAR = 365 * 24 * 60 * 60; // second of one year
+/**
+ * @name calcStableFeeAPR
+ * @description calculate stable fee annual percentage rate. Equation: stableFee = (1 + stableFee) ** (yearSecond * 1000 / expectedBlockTime) - 1
+ * @param {FixedU128} stableFee - loan stable fee on the chain config
+ * @param {number} expectedBlockTime - expected block time on the chain config (precision in milliseconds)
+ * @return {FixedU128} return stable fee annual percentage rate
+ */
+export function calcStableFeeAPR (stableFee: FixedU128, blockTime: number): FixedU128 {
   return FixedU128.fromNatural((1 + stableFee.toNumber()) ** ((YEAR / blockTime) * 1000) - 1);
 }
 
-export function calcRequiredCollateral (
-  debitAmount: FixedU128,
-  requiredCollateralRatio: FixedU128,
-  collateralPrice: FixedU128
-): FixedU128 {
+/**
+ * @name calcRequiredCollateral
+ * @description calculate how many collateral needs. Equation: requiredColalteral = debitAmount * requiredCollateralRatio / collateralPrice
+ * @param {FixedU128} debitAmount - debit amount (USD)
+ * @param {FixedU128} requiredCollateralRatio - required collateral ratio
+ * @param {FixedU128} collateralPrice - collateral price
+ * @returns {FixedU128} return the collateral quantity which is required
+ */
+export function calcRequiredCollateral (debitAmount: FixedU128, requiredCollateralRatio: FixedU128, collateralPrice: FixedU128): FixedU128 {
   if (requiredCollateralRatio.isZero() || collateralPrice.isZero()) {
-    return ZERO;
+    return FixedU128.ZERO;
   }
   return debitAmount.mul(requiredCollateralRatio).div(collateralPrice);
+}
+
+/**
+ * @name calcLiquidationPrice
+ * @description calculate liquidation price. Equation: liquidatioPrice = debitAmount * liquidatioRatio / collateralQuantity
+ * @param {FixedU128} collateralQuantity - collateral quantity
+ * @param {FixedU128} debitAmount - debit amount (USD)
+ * @param {FixedU128} liquidationRatio - required liquidation ratio on the chain config
+ * @returns {FixedU128} return liquidation price
+ */
+export function calcLiquidationPrice (collateralQuantity: FixedU128, debitAmount: FixedU128, liquidationRatio: FixedU128): FixedU128 {
+  return collateralQuantity.isZero() ? FixedU128.ZERO : debitAmount.mul(liquidationRatio).div(collateralQuantity);
+}
+
+/**
+ * @name calcCanGenerate
+ * @description calculate how many stable coins can generate. Equation: canGenerate = (collateralAmount / requiredCollateralRatio - currentDebitAmount) / stableCoinPrice
+ * @param {FixedU128} collateralAmount - collateral amount (USD)
+ * @param {FixedU128} currentDebitAmount - current debit amount in the loan (USD)
+ * @param {FixedU128} requiredCollateralRatio - loan required collateral ratio on the chain config
+ * @param {FixedU128} stableCoinPrice - stable coin price
+ * @param {FixedU128=} slippage - slippage amount, because of can generate amount is reduce every block time.
+ * @returns {FixedU128} return stable coin quantity which can be generated
+ */
+export function calcCanGenerate (collateralAmount: FixedU128, currentDebitAmount: FixedU128, requiredCollateralRatio: FixedU128, stableCoinPrice: FixedU128, slippage?: FixedU128): FixedU128 {
+  if (requiredCollateralRatio.isZero() || stableCoinPrice.isZero()) {
+    return FixedU128.ZERO;
+  }
+  const result = collateralAmount
+    .div(requiredCollateralRatio)
+    .sub(currentDebitAmount)
+    .div(stableCoinPrice);
+  return slippage ? result.sub(slippage) : result;
 }
 
 interface LoanParams {
   debits: FixedU128;
   collaterals: FixedU128;
+  requiredCollateralRatio: FixedU128;
+  stableFee: FixedU128;
+  expectedBlockTime: number;
+  liquidationRatio: FixedU128;
+  collateralPrice: FixedU128;
+  stableCoinPrice: FixedU128;
+  debitExchangeRate: FixedU128;
 }
+
+/**
+ * @class Loan
+ * @classdesc support more structured api, include loan properties and derived properties
+ */
 export class Loan {
-  private _debits: FixedU128;
-  private _collaterals: FixedU128;
+  public debits: FixedU128;
+  public collaterals: FixedU128;
+  public requiredCollateralRatio: FixedU128;
+  public stableFee: FixedU128;
+  public expectedBlockTime: number;
+  public liquidationRatio: FixedU128;
+  public collateralPrice: FixedU128;
+  public stableCoinPrice: FixedU128;
+  public debitExchangeRate: FixedU128;
 
   constructor (params: LoanParams) {
-    this._debits = params.debits;
-    this._collaterals = params.collaterals;
+    const ZERO = FixedU128.ZERO;
+    this.debits = params.debits || ZERO;
+    this.collaterals = params.collaterals || ZERO;
+    this.requiredCollateralRatio = params.requiredCollateralRatio || ZERO;
+    this.stableFee = params.stableFee || ZERO;
+    this.expectedBlockTime = params.expectedBlockTime || 0;
+    this.liquidationRatio = params.liquidationRatio || ZERO;
+    this.collateralPrice = params.collateralPrice || ZERO;
+    this.debitExchangeRate = params.debitExchangeRate || ZERO;
+    this.collateralPrice = params.collateralPrice || ZERO;
+    this.stableCoinPrice = params.stableCoinPrice || ZERO;
   }
 
-  get debits (): FixedU128 {
-    return this._debits;
+  /**
+   * @property {FixedU128} debitAmount
+   */
+  get debitAmount (): FixedU128 {
+    return debitToUSD(this.debits, this.debitExchangeRate, this.stableCoinPrice);
   }
 
-  get collaterels (): FixedU128 {
-    return this._collaterals;
+  /**
+   * @property {FixedU128} collateralAmount
+   */
+  get collateralAmount (): FixedU128 {
+    return collateralToUSD(this.collaterals, this.collateralPrice);
+  }
+
+  /**
+   * @property {FixedU128} collateralRatio
+   */
+  get collateralRatio (): FixedU128 {
+    return calcCollateralRatio(this.collateralAmount, this.debitAmount);
+  }
+
+  /**
+   * @property {FixedU128} stableFeAPR
+   */
+  get stableFeeAPR (): FixedU128 {
+    return calcStableFeeAPR(this.stableFee, this.expectedBlockTime);
+  }
+
+  /**
+   * @property {FixedU128} liquidationPrice
+   */
+  get liquidationPrice (): FixedU128 {
+    return calcLiquidationPrice(this.collaterals, this.debitAmount, this.liquidationRatio);
+  }
+
+  /**
+   * @property {FixedU128} canGenerate
+   */
+  get canGenerate (): FixedU128 {
+    // set slippage to 0.000001
+    return calcCanGenerate(this.collateralAmount, this.debitAmount, this.requiredCollateralRatio, this.stableCoinPrice, FixedU128.fromNatural(0.000001));
+  }
+
+  /**
+   * @property {FixedU128} canPayback
+   */
+  get canPayBack (): FixedU128 {
+    return this.debitAmount;
   }
 }
