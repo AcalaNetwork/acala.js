3b24fdfd8040bcbd06615f7f52087da0907cda14
Xiliang Chen <xlchen1291@gmail.com>
2020-06-08 11:54:34 +1200

    update acala types (#40)

--- packages/types/src/interfaces/cdpEngine/definitions.ts
+++ packages/types/src/interfaces/cdpEngine/definitions.ts
@@ -5,6 +5,34 @@ export default {
         'Auction',
         'Exchange'
       ]
+    },
+    OptionRate: 'Option<Rate>',
+    OptionRatio: 'Option<Ratio>',
+    OptionBalance: 'Option<Balance>',
+    ChangeOptionRate: {
+      _enum: {
+        NoChange: 'Null',
+        NewValue: 'OptionRate'
+      }
+    },
+    ChangeOptionRatio: {
+      _enum: {
+        NoChange: 'Null',
+        NewValue: 'OptionRatio'
+      }
+    },
+    ChangeOptionBalance: {
+      _enum: {
+        NoChange: 'Null',
+        NewValue: 'OptionBalance'
+      }
+    },
+    RiskManagementParams: {
+      maximumTotalDebitValue: 'Balance',
+      stabilityFee: 'Option<Rate>',
+      liquidationRatio: 'Option<Rate>',
+      liquidationPenalty: 'Option<Rate>',
+      requiredCollateralRatio: 'Option<Rate>'
     }
   }
 };
