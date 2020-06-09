afb58608f92bb845cb3061e6c95483ccd6dba34d
Bryan Chen <xlchen1291@gmail.com>
2020-06-10 11:57:48 +1200

    update

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,6 @@
 import './interfaces/augment-api-consts';
 import './interfaces/augment-api-query';
+import './interfaces/augment-api-tx';
 import './interfaces/augment-api';
 import './interfaces/augment-types';
 
@@ -7,9 +8,6 @@ import { RegistryTypes } from '@polkadot/types/types';
 import * as ormlDefinations from '@open-web3/orml-types/interfaces/definitions';
 
 import * as acalaDefinations from './interfaces/definitions';
-
-import * as InterfaceRegistry from './interfaceRegistry';
-export { InterfaceRegistry };
 
 // FIXME: currently we cannot override this in runtime definations because the code generation script cannot handle overrides
 // This will make it behave correctly in runtime, but wrong types in TS defination.
