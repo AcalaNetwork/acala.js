e4dbd1e9c9bf79534197d571e4ed0638a13b76a6
aniiantt <nxiiun@gmail.com>
2020-10-13 11:54:16 +1300

    fix definitions (#66)

--- packages/type-definitions/src/index.ts
+++ packages/type-definitions/src/index.ts
@@ -12,4 +12,3 @@ export { default as primitives } from './primitives';
 export { default as runtime } from './runtime';
 export { default as stakingPool } from './stakingPool';
 export { default as support } from './support';
-export { default as typesBundle } from './types-known/typesBundle';
