afad1cd1d3a5d83d158c8367d038f4fe76cefe11
qiuhao <qiuhao951@gmail.com>
2020-10-28 15:04:40 +0800

    export nft derive

--- packages/api-derive/src/index.ts
+++ packages/api-derive/src/index.ts
@@ -3,7 +3,8 @@ import { DeriveCustom } from '@polkadot/api-derive';
 import * as loan from './loan';
 import * as dex from './dex';
 import * as homa from './homa';
+import * as nft from './nft';
 
-export const derive: DeriveCustom = { loan, dex, homa };
+export const derive: DeriveCustom = { loan, dex, nft, homa };
 
 export * from './types';
