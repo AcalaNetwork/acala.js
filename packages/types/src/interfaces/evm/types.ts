75d0566545a2d3ab1e8ef5fa49deb52a79ef36c4
qwer951123 <qiuhao951@gmail.com>
2020-12-07 17:42:07 +0800

    add EvmAddress type (#89)

---
+++ packages/types/src/interfaces/evm/types.ts
@@ -0,0 +1,9 @@
+// Auto-generated via `yarn polkadot-types-from-defs`, do not edit
+/* eslint-disable */
+
+import { H160 } from '@acala-network/types/interfaces/runtime';
+
+/** @name EvmAddress */
+export interface EvmAddress extends H160 {}
+
+export type PHANTOM_EVM = 'evm';
