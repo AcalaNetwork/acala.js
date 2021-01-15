bfcff28c838f0ea3e40a4172d0b1eb13e6de02e4
Nantian <nxiiun@gmail.com>
2021-01-15 17:18:01 +1300

    bump version

--- packages/type-definitions/src/types-known/versioned.ts
+++ packages/type-definitions/src/types-known/versioned.ts
@@ -1,50 +1,13 @@
 const typesBundleTypes = [
   {
-    minmax: [0, 499] as any,
-    types: {
-      Weight: 'u32',
-      RefCount: 'RefCountTo259',
-      CurrencyId: {
-        _enum: ['ACA', 'AUSD', 'DOT', 'XBTC', 'LDOT', 'RENBTC']
-      }
-    }
-  },
-  {
-    minmax: [500, 599] as any,
-    types: {
-      RefCount: 'RefCountTo259',
-      CurrencyId: {
-        _enum: ['ACA', 'AUSD', 'DOT', 'XBTC', 'LDOT', 'RENBTC']
-      }
-    }
-  },
-  {
     minmax: [600, 699] as any,
-    types: {}
-  },
-  {
-    minmax: [700, 1499] as any,
     types: {
-      Address: 'GenericMultiAddress',
-      LookupSource: 'GenericMultiAddress'
-    }
-  },
-  // Mandala PC1
-  {
-    minmax: [1500, 1599] as any,
-    types: {
-      RefCount: 'RefCountTo259',
-      CurrencyId: {
-        _enum: ['ACA', 'AUSD', 'DOT', 'XBTC', 'LDOT', 'RENBTC']
-      }
+      Address: 'LookupSource',
+      LookupSource: 'IndicesLookupSource'
     }
   },
   {
-    minmax: [1600, 1699] as any,
-    types: {}
-  },
-  {
-    minmax: [1700, undefined] as any,
+    minmax: [700, undefined] as any,
     types: {
       Address: 'GenericMultiAddress',
       LookupSource: 'GenericMultiAddress'
