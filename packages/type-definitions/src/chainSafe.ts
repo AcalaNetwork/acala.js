1ff98f4b849300adedc3ab56a4719f4429b8c5a3
Xiliang Chen <xlchen1291@gmail.com>
2021-06-15 13:32:57 +1200

    fix ChainId (#159)
    * fix ChainId
    
    * update
    
    * update

--- packages/type-definitions/src/chainSafe.ts
+++ packages/type-definitions/src/chainSafe.ts
@@ -2,23 +2,12 @@ export const chainBridge = {
   rpc: {},
   types: {
     DepositNonce: 'u64',
-    ResourceId: '[u8; 32]'
-  },
-  typesAlias: {
-    chainBridge: {
-      ChainId: 'ChainBridgeChainId'
-    }
+    ResourceId: '[u8; 32]',
+    ChainId: 'u8'
   }
 };
 
 export const chainSafeTransfer = {
   rpc: {},
-  types: {
-    ChainBridgeChainId: 'u8'
-  },
-  typesAlias: {
-    chainSafeTransfer: {
-      ChainId: 'ChainBridgeChainId'
-    }
-  }
+  types: {}
 };
