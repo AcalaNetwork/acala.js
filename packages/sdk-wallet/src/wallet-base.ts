06ab0e75ccc06e7dd970595282dd5692be8bd610
qwer951123 <qiuhao951@gmail.com>
2021-07-16 10:09:05 +0800

    feat: update sdk-wallet

--- packages/sdk-wallet/src/wallet-base.ts
+++ packages/sdk-wallet/src/wallet-base.ts
@@ -1,34 +1,123 @@
-import { Token, TokenBalance, MaybeAccount, MaybeCurrency, ObOrPromiseResult } from '@acala-network/sdk-core';
+import {
+  Token,
+  MaybeAccount,
+  MaybeCurrency,
+  ObOrPromiseResult,
+  forceToTokenSymbolCurrencyId,
+  forceToCurrencyIdName,
+  getLPCurrenciesFormName,
+  isDexShare,
+  FixedPointNumber
+} from '@acala-network/sdk-core';
+import { CurrencyId } from '@acala-network/types/interfaces';
 
 import { ApiRx, ApiPromise } from '@polkadot/api';
-import { PriceData, PriceDataWithTimestamp } from './types';
+import { getExistentialDeposit } from './existential-deposit';
+import { BalanceData, PriceData, PriceDataWithTimestamp, TransferConfig } from './types';
 
 export abstract class WalletBase<T extends ApiRx | ApiPromise> {
   protected api: T;
+  protected decimalMap: Map<string, number>;
+  protected currencyIdMap: Map<string, CurrencyId>;
+  protected tokenMap: Map<string, Token>;
+  protected nativeToken!: string;
+  protected runtimeChain!: string;
 
   protected constructor(api: T) {
     this.api = api;
+    this.decimalMap = new Map<string, number>([]);
+    this.currencyIdMap = new Map<string, CurrencyId>([]);
+    this.tokenMap = new Map<string, Token>([]);
+
+    this.init();
+  }
+
+  private init() {
+    const tokenDecimals = this.api.registry.chainDecimals;
+    const tokenSymbol = this.api.registry.chainTokens;
+
+    const defaultTokenDecimal = Number(tokenDecimals?.[0]) || 12;
+
+    this.runtimeChain = this.api.runtimeChain.toString();
+    this.nativeToken = tokenSymbol[0].toString();
+
+    tokenSymbol.forEach((item, index) => {
+      const key = item.toString();
+      const currencyId = forceToTokenSymbolCurrencyId(this.api, key);
+      const decimal = Number(tokenDecimals?.[index]) || defaultTokenDecimal;
+
+      this.decimalMap.set(key, Number(tokenDecimals?.[index]) || defaultTokenDecimal);
+      this.currencyIdMap.set(key, currencyId);
+      this.tokenMap.set(key, Token.fromCurrencyId(currencyId, decimal));
+    });
   }
 
   /**
    * @name getAllTokens
    * @description get all available currencies
    */
-  abstract getAllTokens(): Token[];
+  public getAllTokens(): Token[] {
+    return Array.from(this.tokenMap.values());
+  }
+
+  public getNativeToken(): Token {
+    const nativeCurrencyId = this.api.consts.currencies.getNativeCurrencyId;
+
+    return this.getToken(nativeCurrencyId);
+  }
 
   /**
    * @name getToken
    * @description get the currency
    */
-  abstract getToken(currency: MaybeCurrency): Token;
+  public getToken(currency: MaybeCurrency): Token {
+    const currencyName = forceToCurrencyIdName(currency);
 
-  // get balance
+    if (isDexShare(currencyName)) {
+      const [token1, token2] = getLPCurrenciesFormName(currencyName);
+
+      const _token1 = this.getToken(token1);
+      const _token2 = this.getToken(token2);
+
+      return Token.fromTokens(_token1, _token2);
+    }
+
+    // FIXME: need handle erc20
+
+    return this.tokenMap.get(currencyName) || new Token('mock');
+  }
+
+  public getTransferConfig(currency: MaybeCurrency): TransferConfig {
+    const name = forceToCurrencyIdName(currency);
+
+    if (isDexShare(name)) {
+      const [token1] = Token.sortTokenNames(...getLPCurrenciesFormName(name));
+
+      return {
+        existentialDeposit: getExistentialDeposit(this.runtimeChain, token1)
+      };
+    }
+
+    const existentialDeposit = getExistentialDeposit(this.runtimeChain, forceToCurrencyIdName(currency));
+
+    return { existentialDeposit };
+  }
 
   /**
-   * @name getBalance
+   * @name checkTransfer
+   * @description check transfer amount to target account is ok or not
+   */
+  abstract checkTransfer(
+    account: MaybeAccount,
+    currency: MaybeCurrency,
+    amount: FixedPointNumber
+  ): ObOrPromiseResult<T, boolean>;
+
+  /**
+   * @name queryBalance
    * @description get the balance of the currency
    */
-  abstract queryBalance(account: MaybeAccount, currency: MaybeCurrency): ObOrPromiseResult<T, TokenBalance>;
+  abstract queryBalance(account: MaybeAccount, currency: MaybeCurrency): ObOrPromiseResult<T, BalanceData>;
 
   /**
    * @name queryPrices
