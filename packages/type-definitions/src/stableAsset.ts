5fb4ea82ba7e1f023933836a8a009de1deea6b5f
Frank Yin <ukby.1234@gmail.com>
2021-11-22 00:17:45 -0800

    Update stableAsset.ts

--- packages/type-definitions/src/stableAsset.ts
+++ packages/type-definitions/src/stableAsset.ts
@@ -2,6 +2,23 @@ export default {
   rpc: {},
   types: {
     PoolTokenIndex: 'u32',
-    AssetId: 'CurrencyId'
+    AssetId: 'CurrencyId',
+    StableAssetPoolInfo: {
+      poolAsset: 'CurrencyId',
+      assets: 'Vec<CurrencyId>',
+      precisions: 'Vec<AtLeast64BitUnsigned>',
+      mintFee: 'AtLeast64BitUnsigned',
+      swapFee: 'AtLeast64BitUnsigned',
+      redeemFee: 'AtLeast64BitUnsigned',
+      totalSupply: 'Balance',
+      a: 'AtLeast64BitUnsigned',
+      aBlock: 'BlockNumber',
+      futureA: 'AtLeast64BitUnsigned',
+      futureABlock: 'BlockNumber',
+      balances: 'Vec<Balance>',
+      feeRecipient: 'AccountId',
+      accountId: 'AccountId',
+      palletId: 'AccountId'
+    }
   }
 };
