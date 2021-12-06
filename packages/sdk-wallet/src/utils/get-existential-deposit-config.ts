addd30e66ed77cd74460c1e971c1a92f15509108
qwer951123 <qiuhao951@gmail.com>
2021-12-06 17:52:24 +0800

    feat: add PHA ed (#244)

--- packages/sdk-wallet/src/utils/get-existential-deposit-config.ts
+++ packages/sdk-wallet/src/utils/get-existential-deposit-config.ts
@@ -23,21 +23,23 @@ const EXISTENTIAL_DEPOSIT: ExistentialDepositConfig = {
     RENBTC: MAX,
     CASH: MAX,
     BNC: MAX,
-    VSKSM: MAX
+    VSKSM: MAX,
+    PHA: MAX
   },
   karura: {
     KAR: new FixedPointNumber(0.1, 12),
     KUSD: new FixedPointNumber(0.01, 12),
     KSM: new FixedPointNumber(10 * 0.00001, 12),
     LKSM: new FixedPointNumber(50 * 0.00001, 12),
+    BNC: new FixedPointNumber(800 * 0.00001, 12),
+    VSKSM: new FixedPointNumber(10 * 0.00001, 12),
+    PHA: new FixedPointNumber(4000 * 0.00001, 12),
     ACA: MAX,
     AUSD: MAX,
     DOT: MAX,
     LDOT: MAX,
     RENBTC: MAX,
-    CASH: MAX,
-    BNC: new FixedPointNumber(800 * 0.00001, 12),
-    VSKSM: new FixedPointNumber(10 * 0.00001, 12)
+    CASH: MAX
   },
   dev: {
     KAR: new FixedPointNumber(0.1, 12),
@@ -48,6 +50,7 @@ const EXISTENTIAL_DEPOSIT: ExistentialDepositConfig = {
     AUSD: new FixedPointNumber(0.1, 12),
     DOT: new FixedPointNumber(0.01, 10),
     LDOT: new FixedPointNumber(0.05, 10),
+    PHA: new FixedPointNumber(4000 * 0.00001, 12),
     RENBTC: MAX,
     CASH: MAX,
     BNC: new FixedPointNumber(800 * 0.00001, 12),
