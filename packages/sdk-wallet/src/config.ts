4524a0987c2831ca71c36f13801432f84600b393
qwer951123 <qiuhao951@gmail.com>
2021-12-22 18:34:42 +0800

    support tailKSM feed price

--- packages/sdk-wallet/src/config.ts
+++ packages/sdk-wallet/src/config.ts
@@ -1,1 +1,1 @@
-export const ORACLE_FEEDS_TOKEN = ['DOT', 'XBTC', 'RENBTC', 'POLKABTC', 'KSM', 'KAR'];
+export const ORACLE_FEEDS_TOKEN = ['DOT', 'XBTC', 'RENBTC', 'POLKABTC', 'KSM', 'KAR', 'sa://0'];
