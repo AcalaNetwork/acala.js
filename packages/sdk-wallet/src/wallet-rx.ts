73478eb67ff01ada7b47ef55a8faa54c9d86f63d
qwer951123 <qiuhao951@gmail.com>
2022-01-10 16:14:51 +0800

    update asset registry (#268)
    * update asset registry
    
    * update
    
    * support forign asset ed

--- packages/sdk-wallet/src/wallet-rx.ts
+++ packages/sdk-wallet/src/wallet-rx.ts
@@ -49,21 +49,22 @@ export class WalletRx extends WalletBase<ApiRx> {
     if (!this.api.query?.assetsRegistry?.assetMetadatas) return;
 
     this.api.query.assetRegistry.assetMetadatas.entries().subscribe((data) => {
-      const result = data.map((item) => {
-        const id = item[0]?.args[0].toNumber();
-        const data = (item[1] as Option<AcalaAssetMetadata>).unwrapOrDefault();
+      const result = data
+        .filter((item) => item[0].args[0].isForeignAssetId)
+        .map((item) => {
+          const data = (item[1] as Option<AcalaAssetMetadata>).unwrapOrDefault();
+          const id = item[0]?.args[0].asForeignAssetId.toNumber();
+          const name = createForeignAssetName(id);
+          const decimals = data.decimals.toNumber();
 
-        const name = createForeignAssetName(id);
-        const decimals = Number(data.decimals.toString());
+          const token = Token.fromCurrencyName(name, {
+            decimals,
+            symbol: hexToString(data.symbol.toHex()),
+            ed: FN.fromInner(data.minimalBalance.toString(), decimals)
+          });
 
-        const token = Token.fromCurrencyName(name, {
-          decimals,
-          symbol: hexToString(data.symbol.toHex()),
-          ed: FN.fromInner(data.minimalBalance.toString(), decimals)
+          return [name, token] as const;
         });
-
-        return [name, token] as const;
-      });
 
       this.assetMetadatas$.next(new Map(result));
     });
