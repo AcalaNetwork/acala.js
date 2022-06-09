097e314df5dbc133f83e0489dfa541a51cec80ec
qwer951123 <qiuhao951@gmail.com>
2022-06-09 19:09:43 +0800

    remove api.const.honzon.collateralCurrencyIds

--- packages/api-derive/src/utils/index.ts
+++ packages/api-derive/src/utils/index.ts
@@ -1,8 +1,12 @@
 import { AcalaPrimitivesCurrencyCurrencyId } from '@acala-network/types/interfaces/types-lookup';
 import { ApiInterfaceRx } from '@polkadot/api/types';
-import { Vec } from '@polkadot/types';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-export function getAllCollateralCurrencyIds(api: ApiInterfaceRx): Vec<AcalaPrimitivesCurrencyCurrencyId> {
-  return (api.consts.cdpEngine.collateralCurrencyIds ||
-    api.consts.honzon.collateralCurrencyIds) as any as Vec<AcalaPrimitivesCurrencyCurrencyId>;
+export function getAllCollateralCurrencyIds(api: ApiInterfaceRx): Observable<AcalaPrimitivesCurrencyCurrencyId[]> {
+  return api.query.cdpEngine.collateralParams.keys().pipe(
+    map((keys) => {
+      return keys.map((item) => item.args[0]);
+    })
+  );
 }
