315b33d31bda7e021ff02ca6a0076a186f223e97
qwer951123 <qiuhao951@gmail.com>
2023-07-27 16:48:26 +0800

    fix lint

--- packages/sdk/src/wallet/utils/to-promise.ts
+++ packages/sdk/src/wallet/utils/to-promise.ts
@@ -1,6 +1,6 @@
-import { Observable, firstValueFrom, isObservable } from "rxjs";
+import { Observable, firstValueFrom, isObservable } from 'rxjs';
 
-export function toPromise <T extends any>(a: Observable<T> | Promise<T>): Promise<T> {
+export function toPromise<T>(a: Observable<T> | Promise<T>): Promise<T> {
   if (isObservable(a)) return firstValueFrom(a);
 
   return a;
